1db8599e0210e9148a70c3af5abbd54d77cc0d75
botondbutuza <str7418880>
2017-04-11 10:55:45 +0100

    self-close

--- src/main/java/uk/co/botondbutuza/minglbanner/MinglBannerImpl.java
+++ src/main/java/uk/co/botondbutuza/minglbanner/MinglBannerImpl.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.graphics.Color;
+import android.os.Handler;
 import android.support.annotation.AttrRes;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
@@ -22,8 +23,11 @@ import android.widget.TextView;
 
 public class MinglBannerImpl extends FrameLayout implements MinglBanner {
     private static final @ColorInt int INFO_BG_COLOUR = Color.GRAY;
+
+    private static final int DISMISS_TIMEOUT = 2000;
     private static final int ANIM_DURATION = 400;
 
+    private Handler handler = new Handler();
     private View left, right, textContainer;
     private TextView text;
     private Button button;
@@ -85,14 +89,18 @@ public class MinglBannerImpl extends FrameLayout implements MinglBanner {
     public void show() {
         left.animate().translationX(0).setDuration(ANIM_DURATION).start();
         right.animate().translationX(0).setDuration(ANIM_DURATION).start();
-        text.getHandler().postDelayed(this::showText, ANIM_DURATION / 2);
+
+        handler.postDelayed(this::showText, ANIM_DURATION / 2);
+        handler.postDelayed(this::dismiss, DISMISS_TIMEOUT);
     }
 
     @Override
     public void dismiss() {
+        handler.removeCallbacksAndMessages(null);
+
         left.animate().translationX(-getWidth() / 2).setDuration(ANIM_DURATION).start();
         right.animate().translationX(getWidth()).setDuration(ANIM_DURATION).start();
-        text.getHandler().postDelayed(this::dismissText, ANIM_DURATION / 3);
+        handler.postDelayed(this::dismissText, ANIM_DURATION / 3);
     }
 
     @Override
@@ -101,7 +109,10 @@ public class MinglBannerImpl extends FrameLayout implements MinglBanner {
         withText(message);
 
         button.setText(action.getText());
-        button.setOnClickListener(action);
+        button.setOnClickListener(v -> {
+            dismiss();
+            action.onClick(v);
+        });
 
         show();
     }
